fdf9c28ac9cdd63f4cfcddbbfe2e146c1014f1da
Ethan Holz <ethan.holz@omsf.io>
2026-03-03 11:18:12 -0700

    fix: clone correct openfe branch

--- cloud_init.sh
+++ cloud_init.sh
@@ -20,7 +20,7 @@ curl -fsSL https://pixi.sh/install.sh | bash
 export PATH="\$HOME/.pixi/bin:\$PATH"
 
 git clone https://github.com/omsf-eco-infra/benchmarking-orchestration.git
-git clone https://github.com/OpenFreeEnergy/performance_benchmarks.git
+git clone -b industry_benchmarks --single-branch https://github.com/OpenFreeEnergy/performance_benchmarks.git
 CLI_PATH="\$HOME/benchmarking-orchestration"
 pixi install --manifest-path "\$CLI_PATH/pyproject.toml" -e bench
 
